fb5322c3b42b3c82d189948d83164bca80e1a31f
Paludeto <gpalu54@gmail.com>
2025-04-11 10:39:00 -0300

    ap2 inicio

---
+++ ap2/ap2.m
@@ -0,0 +1,36 @@
+t = 0:0.001:5;
+z1 = 5 * exp(4j * pi * t);
+z2 = 2 * exp(-1j * 2 * pi * t);
+
+z3 = (z1 + z2) / 2;
+
+plot(real(t), imag(z3));
+% saveas(gcf, "img/z3(t).png")
+
+plot3(real(z3), imag(z3), t);
+% saveas(gcf, "img/z3(t)_3d.png")
+
+t1 = 0:0.01:5;
+x1 = real(10 * exp((-1 - 2j * pi) * t1));
+plot(t1, x1);
+% saveas(gcf, "img/x1(t).png")
+
+t2 = 0:0.01:6;
+x2 = imag(3 - exp((-2 - 4j * pi) * t2));
+plot(t2, x2);
+% saveas(gcf, "img/x2(t).png")
+
+t3 = 0:0.01:4;
+x3 = 3 - imag(exp((-4 + pi * j) * t3));
+plot(t3, x3);
+% saveas(gcf, "img/x3(t).png")
+
+t4 = 0:0.01:7;
+x4 = zeros(size(t4));   % Inicializa um vetor preenchido com 0s, do tamanho de t4
+
+for k = 1:5
+    x4 = x4 + cos(2 * pi * k * t4 + pi / 6);    % Para k de 1 a 5,
+end
+
+plot(t4, x4);
+% saveas(gcf, "img/x4(t).png")
